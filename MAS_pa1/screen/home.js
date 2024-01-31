131f4d0a06ce603296eecf29c30db538f4c31d19
Xingfang "Eliot" Yuan <xf.e.yuan@gmail.com>
2024-01-30 21:49:06 -0500

    implemented and successfully ran the stacked screens on expo

---
+++ MAS_pa1/screen/home.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
