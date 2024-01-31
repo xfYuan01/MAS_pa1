45bd15b76f51b00fe3ab401582097be7d12ed95f
Xingfang "Eliot" Yuan <xf.e.yuan@gmail.com>
2024-01-30 22:16:21 -0500

    made the minimal UIs for login

--- MAS_pa1/screen/login.js
+++ MAS_pa1/screen/login.js
@@ -1,14 +1,51 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native'
 
 const Login = () => {
     return (
-        <View>
-            <Text>Login Screen</Text>
-        </View>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding"
+        >
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Email"
+                    style={styles.input}
+              />
+
+              <TextInput
+                    placeholder="Password"
+                    style={styles.input}
+                    secureTextEntry
+              />
+            </View>
+  
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={() => {}}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity
+                    onPress={() => {}}
+                    style={[styles.button, styles.buttonOutline]}
+                >
+                    <Text style={styles.buttonOutlineText}>Register</Text>
+                </TouchableOpacity>
+            </View>
+      </KeyboardAvoidingView>
     )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center',
+
+    }
+})
